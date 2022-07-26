e2a60e120dc8b9ab617e57c681af335ae21b6c00
Roseventrue <ventruerose@gmail.com>
2022-07-26 11:14:38 -0300

    frist commit

---
+++ src/Programa/um.java
@@ -0,0 +1,15 @@
+package Programa;
+
+import java.nio.channels.ScatteringByteChannel;
+
+public class um {
+    public static void main(String[] args) {
+        Gato gato =new gato ();
+        System.out.println(gato);
+        /*int a =5;
+        int b =3;
+        System.out.println("hello world" + (a+b));*/
+
+
+    }
+}
